3fd4981c9dd7a3b22587588d16297e6e726bbb0c
mkeaudric <mikeaudric123@gmail.com>
2024-09-21 11:29:09 +0700

    First commit

---
+++ penjumlahan.py
@@ -0,0 +1,7 @@
+def penjumlahan():
+    a = int(input("masukin angka pertama : "))
+    b = int(input("masukin angka kedua : "))
+    return a+b
+
+hasil = penjumlahan()
+print("Hasil penjumlahan:", hasil)
